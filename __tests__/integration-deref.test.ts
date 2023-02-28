5522c91f014862fc1248ffc8531781bd87486c9a
Daniel Oaks <daniel@danieloaks.net>
2023-02-28 19:58:43 +1000

    Make test more sensible and nice

--- __tests__/integration-deref.test.ts
+++ __tests__/integration-deref.test.ts
@@ -1,15 +1,17 @@
-import * as tmp from 'tmp';
 import fs from 'fs';
 import { join } from 'path';
+import * as tmp from 'tmp';
 
-import { derefOAS } from '../src/lib/convertOAS'
+import { expect, test } from '@jest/globals';
+
+import { derefOAS } from '../src/lib/convertOAS';
 
 test('dereferences OpenAPI file correctly', done => {
   // get tmp file
   const tmpobj = tmp.fileSync();
 
   derefOAS(join(__dirname, 'openapi-1.yaml'), tmpobj.name, ['system-messages'], () => {
-    expect(fs.readFileSync(tmpobj.name)).toEqual(fs.readFileSync(join(__dirname, 'openapi-1-dereferenced-sysmsg.json')));
+    expect(JSON.parse(fs.readFileSync(tmpobj.name, 'utf8'))).toEqual(JSON.parse(fs.readFileSync(join(__dirname, 'openapi-1-dereferenced-sysmsg.json'), 'utf8')));
 
     tmpobj.removeCallback();
 
