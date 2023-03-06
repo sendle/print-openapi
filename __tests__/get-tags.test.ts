e9946ca65cfc0707b2bebc9689fd3fdd4d5169ff
Daniel Oaks <daniel@danieloaks.net>
2023-03-07 08:58:18 +1000

    Fix tags test

--- __tests__/get-tags.test.ts
+++ __tests__/get-tags.test.ts
@@ -2,29 +2,17 @@ import { join } from 'path';
 
 import { expect, test } from '@jest/globals';
 
-import { getOASTags, Tag } from '../src/lib/convertOAS';
+import { getOASTags } from '../src/lib/convertOAS';
 
-test('gets OpenAPI tags', done => {
-  getOASTags(join(__dirname, 'openapi-1.yaml'), tags => {
-    try {
-      expect(tags).toEqual([
-        {
-          Tag: "moderation",
-          Description: "API used by moderators and admins.",
-        },
-        {
-          Tag: "system-messages",
-          Description: "Post system messages.",
-        },
-      ]);
-      done();
-    } catch (error) {
-      if (error instanceof Error || typeof error === 'string') {
-        done(error);
-      } else {
-        // convert it first to stop typescript from yelling
-        done(String(error));
-      }
-    }
-  });
+test('gets OpenAPI tags', () => {
+  expect(getOASTags(join(__dirname, 'openapi-1.yaml'))).resolves.toEqual([
+    {
+      Tag: "moderation",
+      Description: "API used by moderators and admins.",
+    },
+    {
+      Tag: "system-messages",
+      Description: "Post system messages.",
+    },
+  ]);
 });
